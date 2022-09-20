41226f7bb6b9822510697abccb5ce2079f980167
schulz-franco <fschulz2222@gmail.com>
2022-09-19 21:18:39 -0300

    search

--- src/components/productos/buscar.jsx
+++ src/components/productos/buscar.jsx
@@ -1,10 +1,21 @@
+import { useContext } from "react"
+import { ProductosContext } from "../../context/productosContext"
+
+const onSubmitHandler = (ev, setSearch)=> {
+  ev.preventDefault()
+  setSearch(ev.target.firstElementChild.value.trim())
+}
+
 const BuscarProducto = ()=> {
-    return(
-      <form action="" className="buscar">
-        <input placeholder="Buscar un producto..." type="text" maxLength={30} minLength={2} required={false} />
-        <button>Buscar</button>
-      </form>
-    )
+
+  const { search, setSearch } = useContext(ProductosContext)
+
+  return(
+    <form action="" className="buscar" onSubmit={(ev)=> onSubmitHandler(ev, setSearch)}>
+      <input placeholder="Buscar un producto..." type="text" maxLength={30} minLength={2} required={false} />
+      <button>Buscar</button>
+    </form>
+  )
 }
 
 export default BuscarProducto
--- src/context/productosContext.js
+++ src/context/productosContext.js
@@ -7,19 +7,31 @@ export const ProductosContext = createContext()
 export const ProductosContextProvider = ({ children })=> {
 
     const [productos, setProductos] = useState(null)
+    const [search, setSearch] = useState("")
 
-    const value = {productos, setProductos}
+    const value = {productos, setProductos, search, setSearch}
 
     useEffect(()=> {
-        const unsub = database.collection("productos").onSnapshot(snap => {
-            let collection = []
-            snap.forEach(doc => {
-                collection.push(doc.data())
+        if (search === "") {
+            const unsub = database.collection("productos").onSnapshot(snap => {
+                let collection = []
+                snap.forEach(doc => {
+                    collection.push(doc.data())
+                })
+                setProductos(collection)
             })
-            setProductos(collection)
-        })
-        return () => unsub()
-    }, [])
+            return () => unsub()
+        } else {
+            const unsub = database.collection("productos").where("producto", "==", search).onSnapshot(snap => {
+                let collection = []
+                snap.forEach(doc => {
+                    collection.push(doc.data())
+                })
+                setProductos(collection)
+            })
+            return () => unsub()
+        }
+    }, [search])
 
     return (
         <ProductosContext.Provider value={value}>
