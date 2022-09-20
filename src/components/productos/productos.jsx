7574f725de768b630e37d090dfd12d3d18b4973b
schulz-franco <fschulz2222@gmail.com>
2022-09-20 19:59:24 -0300

    modal editar producto

--- src/components/productos/productos.jsx
+++ src/components/productos/productos.jsx
@@ -2,9 +2,10 @@ import { useState, useContext } from "react"
 import { CgClose } from "react-icons/cg"
 import { GoPencil } from "react-icons/go"
 import { ProductosContext } from "../../context/productosContext"
-import { eliminarProducto } from "../../firebase/methods"
+import { eliminarProducto, editarProducto } from "../../firebase/methods"
 
-const onClickModalCancelHandler = (setModal)=> {
+const onClickModalCancelHandler = (ev, setModal)=> {
+    ev.preventDefault()
     setModal([false, null])
 }
 
@@ -13,15 +14,65 @@ const onClickModalAcceptHandler = (setModal, id)=> {
     setModal([false, null])
 }
 
-const Modal = ({ mode, setModal, id })=> {
+const onSubmitModalEditHandler = (ev, setError, setModal, id)=> {
+    ev.preventDefault()
+  
+    if (ev.target.childNodes[0].lastElementChild.value.trim() === "") {
+      ev.target.childNodes[0].lastElementChild.classList.add("error")
+      return setError([true, "El campo 'Producto' esta vacío."])
+    } else {
+      ev.target.childNodes[0].lastElementChild.classList.remove("error")
+      setError([false, ""])
+    }
+  
+    let datos = []
+    for (let counter = 0; counter < 5; counter++) {
+      datos.push(ev.target.childNodes[counter].lastElementChild.value.trim())
+      ev.target.childNodes[counter].lastElementChild.value = ""
+    }
+  
+    if (datos[1] === "") datos[1] = "-" 
+  
+    let producto = {
+      producto: datos[0],
+      descripcion: datos[1],
+      precio: datos[2],
+      stock: datos[3],
+      iva: datos[4]
+    }
+  
+    setModal([false, null])
+    console.log(producto)
+    editarProducto(id, producto)
+  }
+
+const Modal = ({ mode, setModal, id, producto })=> {
+
+    const [error, setError] = useState([false, null])
+
     if (mode === "delete") {
         return(
             <div className="modal-container">
-                <div className="modal">
-                    <span>¿Eliminar producto?</span>
+                <div className="modal modal-delete">
+                    <span className="acceptMessage">¿Eliminar producto?</span>
                     <button onClick={()=> onClickModalAcceptHandler(setModal, id)} className="accept">Confirmar</button>
-                    <button onClick={()=> onClickModalCancelHandler(setModal)} className="cancel">Cancelar</button>
+                    <button onClick={(ev)=> onClickModalCancelHandler(ev, setModal)} className="cancel">Cancelar</button>
                 </div>
+            </div>
+        )
+    } else if (mode === "edit") {
+        return(
+            <div className="modal-container">
+                <form onSubmit={(ev)=> onSubmitModalEditHandler(ev, setError, setModal, id)} className="modal modal-edit">
+                    <label>Producto<input defaultValue={producto.producto} placeholder="Producto" required maxLength={40} minLength={3} type="text" /></label>
+                    {error[0] && <span className="errorMessage">{error[1]}</span>}
+                    <label>Descripción<input defaultValue={producto.descripcion} placeholder="Descripción" maxLength={50} type="text" /></label>
+                    <label>Precio<input defaultValue={producto.precio} placeholder="Precio" required maxLength={12} minLength={1} type="number" /></label>
+                    <label>Stock<input defaultValue={producto.stock} placeholder="Stock" required maxLength={6} minLength={1} type="number" /></label>
+                    <label>P. Iva<input defaultValue={producto.iva} placeholder="P. Iva" required maxLength={2} minLength={2} type="number" /></label>
+                    <button type="submit">Confirmar</button>
+                    <button onClick={(ev)=> onClickModalCancelHandler(ev, setModal)} className="accept">Cancelar</button>
+                </form>
             </div>
         )
     }
@@ -34,7 +85,7 @@ const Producto = ({ id, producto })=> {
     return(
         <div>
             {modal[0] && 
-                <Modal mode={modal[1]} setModal={setModal} id={id} />
+                <Modal mode={modal[1]} setModal={setModal} id={id} producto={producto}/>
             }
             <div className="producto">
                 <span className="name">{producto.producto}</span>
@@ -42,11 +93,11 @@ const Producto = ({ id, producto })=> {
                 <span className="price">$ {producto.precio}</span>
                 <span className="number">{producto.stock}</span>
                 <span className="number">{producto.iva}%</span>
-                <button className="edit">
+                <button onClick={()=> setModal([true, "edit"])} className="edit">
                     <GoPencil className="icon" />
                 </button>
-                <button className="delete">
-                    <CgClose onClick={()=> setModal([true, "delete"])} className="icon" />
+                <button onClick={()=> setModal([true, "delete"])} className="delete">
+                    <CgClose className="icon" />
                 </button>
             </div>
         </div>
--- src/firebase/methods.js
+++ src/firebase/methods.js
@@ -10,6 +10,16 @@ export const nuevoProducto = async ({ producto, descripcion, precio, stock, iva 
     })
 }
 
+export const editarProducto = async (id, { producto, descripcion, precio, stock, iva })=> {
+    database.collection("productos").doc(id).update({
+        producto: producto,
+        descripcion: descripcion,
+        precio: precio,
+        stock: stock,
+        iva: iva
+    })
+}
+
 export const eliminarProducto = async (id) => {
     database.collection("productos").doc(id).delete()
 }
